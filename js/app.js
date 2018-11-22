d3e4b912bafa058fe1616cc1f64897a38946e451
EvyHaan <evy.haan@gmail.com>
2018-11-21 21:29:03 -0800

    begin adding selector keywords

--- js/app.js
+++ js/app.js
@@ -7,26 +7,36 @@ function HornedAnimal(obj) {
   this.keyword = obj.keyword;
   this.description = obj.description;
 
-  allAnimals.push(this);
+	allAnimals.push(this);
+	allKeywords.push(this.keyword);
 }
 
 const allAnimals = [];
+const allKeywords =[];
+console.log(allKeywords);
 
 HornedAnimal.prototype.render = function() {
-	$('main').append('<section class="clone"><section>');
-	let $clone = $('section[class="clone"]');
+  $('main').append('<section class="clone"><section>');
+  let $clone = $('section[class="clone"]');
 
-	let animalTemplate = $('#photo-template').html();
+  let animalTemplate = $('#photo-template').html();
 
-	$clone.html(animalTemplate);
+  $clone.html(animalTemplate);
 
-	$clone.find('h2').text(this.title);
-	$clone.find('p').text(this.description);
-	$clone.find('img').attr('src', this.image_url);
+  $clone.find('h2').text(this.title);
+  $clone.find('p').text(this.description);
+  $clone.find('img').attr('src', this.image_url);
 
-	$clone.removeClass('clone');
-	$clone.attr('class', this.name);
+  $clone.removeClass('clone');
+  $clone.attr('class', this.name);
+}
 
+HornedAnimal.prototype.selectByKeyword = function() {
+
+// loop through the array
+// pick the keyword of each obj
+// append keyword to the select element
+// render only those images to the page or hide all other images
 }
 
 function readJson() {

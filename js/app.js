c51bab72e1bfff31c58051149c92868c1705d496
EvyHaan <evy.haan@gmail.com>
2018-11-27 12:46:50 -0800

    trying to reach the  photo-template div to add a keyword class for each object

--- js/app.js
+++ js/app.js
@@ -26,6 +26,8 @@ HornedAnimal.prototype.render = function() {
 };
 
 HornedAnimal.prototype.selectByKeyword = function() {
+  $('#photo-template').addClass(`${this.keyword}`);
+  
   allAnimals.forEach( (animalObj) => { // for each new horned animal object instance, do the following...
     if (!allKeywords.includes(animalObj.keyword)) { // check each keyword against any in the keyword array, to eliminate duplicates
       allKeywords.push(animalObj.keyword); //if the keyword is not in the array already, add it
@@ -70,24 +72,25 @@ function readJson2() {
 
 $('select').on('change', function () {
   let $selection = $(this).val();
+
   if($selection === 'default') {
-    $('div').show();
+    $('#photo-template').show();
     return;
   }
-  $('div').hide();
-  $(`div[class = "${$selection}"]`).show();
+  $('#photo-template').hide();
+  $(`#photo-template[class = "${$selection}"]`).show();
 });
 
 //Click Function for Button to change pages
 $('#button1').on('click',function(){
-  $('div').remove();
+  $('#photo-template').remove();
   $('.words').remove();
   $(() => readJson());
 })
 
 //Click Handler for Button 2
 $('#button2').on('click',function(){
-  $('div').remove();
+  $('#photo-template').remove();
   $('.words').remove();
   $(() => readJson2());
 })
